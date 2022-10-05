2bc40efe0981cb10778bbaf12b3f2df6b930970a
Ede Success <edesuccess54@gmail.com>
2022-10-05 12:26:11 +0100

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mern-task-app-xqcu.onrender.com/"]
+}))
 app.use("/api/tasks",taskRoutes);
 
 // Routes 

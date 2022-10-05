fffd67e66cda62062b4f73760128c18c0e7f4f7e
Ede Success <edesuccess54@gmail.com>
2022-10-05 12:05:05 +0100

    removed orign from cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use(cors({
-    origin: ["http://localhost:3000", "https://mern-task-app-xqcu.onrender.com"]
-}))
+app.use(cors())
 app.use("/api/tasks",taskRoutes);
 
 // Routes 
